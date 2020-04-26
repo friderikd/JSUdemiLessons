412e6a61f3caad5ee4ce66a241369de130fb75be
friderikd <friderikdjubin@gmail.com>
2020-04-26 23:38:28 +0300

    task12 updated

--- task12/js/second.js
+++ task12/js/second.js
@@ -1,7 +1,20 @@
-let age = document.getElementById('age');
- 
-function showUser(surname, name) {
-          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
- }
- 
- showUser.apply(age, ["Горький","Максим"]);
+class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
+
+	createDiv() {
+		let elem = document.createElement('div');
+		document.body.appendChild(elem);
+		let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+		elem.style.cssText = param;
+	}
+}
+
+const item = new Options(300, 350, "red", 14, "center");
+
+item.createDiv();
